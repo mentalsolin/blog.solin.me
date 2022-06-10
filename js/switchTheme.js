56b35d595b088ef460fa893691468f404248523d
The Solin <solinsolinov@gmail.com>
2022-06-10 21:45:21 +0200

    release-1.3-6
    Added manual theme
    bug fixes

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,33 +1,42 @@
-const switchToLight = document.getElementById("switchToLight")
-const switchToDark = document.getElementById("switchToDark")
-
-if (localStorage.getItem('theme', 'dark')) {
-    document.body.classList.add("dark_theme")
-}
-else {
-    document.body.classList.remove("dark_theme")
-}
-
 const userTheme = window.matchMedia('(prefers-color-scheme: dark)')
-setInterval(displayHello, 100);
+const themeSelector = document.getElementById('themeSelector')
 
-function displayHello() {
-    userThemeFunc = userTheme.matches
-    if (userTheme.matches === false) {
-        document.body.classList.remove("dark_theme")
-        localStorage.removeItem('theme', 'dark')
+if (localStorage.getItem('firstTheme') === 'dark') {
+    document.body.classList.add('dark')
+}
+if (localStorage.getItem('firstTheme') === 'light') {
+    document.body.classList.remove('dark')
+}
+
+setInterval(themeDetect, 1);
+function themeDetect() {
+    if (localStorage.getItem('staticTheme', 'device')) {
+        if (userTheme.matches === false) {
+            document.body.classList.remove('dark')
+        }
+        else {
+            document.body.classList.add('dark')
+        }
     }
+}
+
+function themeSelectorFunc() {
+    let selectIndex = themeSelector.selectedIndex
     
-    else {
-        document.body.classList.add("dark_theme")
-        localStorage.setItem('theme', 'dark')
+    if (selectIndex === 0) {
+        localStorage.setItem('staticTheme', 'device')
+        localStorage.setItem('firstTheme', 'device')
     }
 
-    function swtichToLight() {
-        document.body.classList.add("dark_theme")
+    if (selectIndex === 1) {
+        document.body.classList.add('dark')
+        localStorage.removeItem('staticTheme', 'device')
+        localStorage.setItem('firstTheme', 'dark')
     }
-    
-    function swtichToDark() {       
-        document.body.classList.remove("dark_theme")
+
+    if (selectIndex === 2) {
+        document.body.classList.remove('dark')
+        localStorage.removeItem('staticTheme', 'device')
+        localStorage.setItem('firstTheme', 'light')
     }
 }
