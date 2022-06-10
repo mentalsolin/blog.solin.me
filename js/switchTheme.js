febf590fe0bda93c2b37b46e1b80232443e9848b
The Solin <solinsolinov@gmail.com>
2022-06-10 10:42:11 +0200

    release-1.3-5
    Bug fix

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,19 +1,26 @@
 const switchToLight = document.getElementById("switchToLight")
 const switchToDark = document.getElementById("switchToDark")
 
+if (localStorage.getItem('theme', 'dark')) {
+    document.body.classList.add("dark_theme")
+}
+else {
+    document.body.classList.remove("dark_theme")
+}
+
 const userTheme = window.matchMedia('(prefers-color-scheme: dark)')
 setInterval(displayHello, 100);
 
 function displayHello() {
     userThemeFunc = userTheme.matches
-
     if (userTheme.matches === false) {
-        console.log("Theme 2")
         document.body.classList.remove("dark_theme")
+        localStorage.removeItem('theme', 'dark')
     }
     
     else {
         document.body.classList.add("dark_theme")
+        localStorage.setItem('theme', 'dark')
     }
 
     function swtichToLight() {
