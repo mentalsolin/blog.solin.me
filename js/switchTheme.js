820a88d54d766d0900558be6b072def62527a9b5
The Solin <solinsolinov@gmail.com>
2022-06-11 08:13:09 +0200

    release-1.3-6-3
    Bug fixes

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -1,6 +1,12 @@
 const userTheme = window.matchMedia('(prefers-color-scheme: dark)')
 const themeSelector = document.getElementById('themeSelector')
 
+if (localStorage.getItem('firstTheme') === 'device-light') {
+    document.body.classList.remove('dark')
+}
+if (localStorage.getItem('firstTheme') === 'device-dark') {
+    document.body.classList.add('dark')
+}
 if (localStorage.getItem('firstTheme') === 'dark') {
     document.body.classList.add('dark')
 }
@@ -8,14 +14,16 @@ if (localStorage.getItem('firstTheme') === 'light') {
     document.body.classList.remove('dark')
 }
 
-setInterval(themeDetect, 1);
+setInterval(themeDetect, 100);
 function themeDetect() {
-    if (localStorage.getItem('staticTheme', 'device')) {
+    if (localStorage.getItem('dynamicTheme', 'true')) {
         if (userTheme.matches === false) {
             document.body.classList.remove('dark')
+            localStorage.setItem('firstTheme','device-light')
         }
         else {
             document.body.classList.add('dark')
+            localStorage.setItem('firstTheme','device-dark')
         }
     }
 }
@@ -23,20 +31,20 @@ function themeDetect() {
 function themeSelectorFunc() {
     let selectIndex = themeSelector.selectedIndex
     
-    if (selectIndex === 0) {
-        localStorage.setItem('staticTheme', 'device')
+    if (selectIndex === 1) {
+        localStorage.setItem('dynamicTheme', 'true')
         localStorage.setItem('firstTheme', 'device')
     }
 
-    if (selectIndex === 1) {
+    if (selectIndex === 2) {
         document.body.classList.add('dark')
-        localStorage.removeItem('staticTheme', 'device')
+        localStorage.removeItem('dynamicTheme', 'true')
         localStorage.setItem('firstTheme', 'dark')
     }
 
-    if (selectIndex === 2) {
+    if (selectIndex === 3) {
         document.body.classList.remove('dark')
-        localStorage.removeItem('staticTheme', 'device')
+        localStorage.removeItem('dynamicTheme', 'true')
         localStorage.setItem('firstTheme', 'light')
     }
 }
