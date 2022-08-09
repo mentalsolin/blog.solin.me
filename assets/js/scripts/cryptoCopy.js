7d17da3c83f9a0e52672dfb54c2f3561795a60f8
The Solin <solinsolinov@gmail.com>
2022-08-09 04:31:06 +0200

    Rework testing

--- assets/js/scripts/cryptoCopy.js
+++
@@ -1,7 +0,0 @@
-function copyMonero() {
-    navigator.clipboard.writeText('8AdrcJgoMux2pKMapcd2ACJJ5bKFdx5trMRMiFt8Ph8rDH4dZVMZZr9iXJRV95gdbH1eBQNwppkMq6Uhz8KjRFEV1VBT5F2');
-}
-
-function copyBitcoin() {
-    navigator.clipboard.writeText('1BDbKDhf199NjDv23khdcKzbNNYEyzgK9');
-}
